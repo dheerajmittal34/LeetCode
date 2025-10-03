ae9e648fba1a46c2e56ad2ce0da4a53e1c4ea17d
Dheeraj Mittal <dheerajmittal098@gmail.com>
2025-10-03 10:30:20 +0530

    Using Iteration

---
+++ MatrixSearch.java
@@ -0,0 +1,41 @@
+public class MatrixSearch {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        if (m == 0) return false;
+        int n = matrix[0].length;
+
+        int row = 0;
+        int col = n - 1;
+
+        while (row < m && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                col--;  // Move left
+            } else {
+                row++;  // Move down
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 4, 7, 11, 15},
+            {2, 5, 8, 12, 19},
+            {3, 6, 9, 16, 22},
+            {10, 13, 14, 17, 24},
+            {18, 21, 23, 26, 30}
+        };
+
+        int target = 5;
+
+        boolean found = searchMatrix(matrix, target);
+
+        if (found) {
+            System.out.println("Target " + target + " found in the matrix.");
+        } else {
+            System.out.println("Target " + target + " not found in the matrix.");
+        }
+    }
+}
